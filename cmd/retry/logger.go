fd21f383262dc909924f24c9798b2390845ba01c
Kamil Samigullin <kamil@samigullin.info>
2017-05-02 10:55:19 +0300

    up test coverage

--- cmd/retry/logger.go
+++ cmd/retry/logger.go
@@ -22,33 +22,33 @@ type logger struct {
 
 func (l *logger) Debug(message string) {
 	if l.debug {
-		l.stdout.Print(colorize(yellow, "[DEBUG]") + " " + message)
+		l.stdout.Print(l.colorize(yellow, "[DEBUG]") + " " + message)
 	}
 }
 
 func (l *logger) Debugf(format string, args ...interface{}) {
 	if l.debug {
-		l.stdout.Printf(colorize(yellow, "[DEBUG]")+" "+format, args...)
+		l.stdout.Printf(l.colorize(yellow, "[DEBUG]")+" "+format, args...)
 	}
 }
 
 func (l *logger) Error(message string) {
-	l.stderr.Print(colorize(red, "[ERROR]") + " " + message)
+	l.stderr.Print(l.colorize(red, "[ERROR]") + " " + message)
 }
 
 func (l *logger) Errorf(format string, args ...interface{}) {
-	l.stderr.Printf(colorize(red, "[ERROR]")+" "+format, args...)
+	l.stderr.Printf(l.colorize(red, "[ERROR]")+" "+format, args...)
 }
 
 func (l *logger) Info(message string) {
-	l.stdout.Print(colorize(green, "[INFO]") + " " + message)
+	l.stdout.Print(l.colorize(green, "[INFO]") + " " + message)
 }
 
 func (l *logger) Infof(format string, args ...interface{}) {
-	l.stdout.Printf(colorize(green, "[INFO]")+" "+format, args...)
+	l.stdout.Printf(l.colorize(green, "[INFO]")+" "+format, args...)
 }
 
-func colorize(color int, str string) string {
+func (l *logger) colorize(color int, str string) string {
 	if NoColor {
 		return str
 	}
