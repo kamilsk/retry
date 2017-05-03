c18ab2a3df363a5020213ec9e8a13bb7418172d1
Kamil Samigullin <kamil@samigullin.info>
2017-05-03 21:20:54 +0300

    update with master

--- cmd/retry/main.go
+++ cmd/retry/main.go
@@ -6,11 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"time"
 
 	"github.com/kamilsk/retry"
-	"github.com/kamilsk/retry/cmd/retry/flag"
-	"github.com/kamilsk/retry/strategy"
 )
 
 var (
@@ -54,54 +51,10 @@ func main() {
 		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, &buf{c: done, w: os.Stderr}
 		return cmd.Run()
 	}
-	ctx, cancel := ctx(timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	if err := retry.Retry(ctx, action, strategies...); err != nil {
 		l.Errorf("error occcured: %q", err)
 		close(done)
 	}
 	cancel()
-}
-
-func parse() (context.Context, []string, []strategy.Strategy) {
-	cl := flag.NewFlagSet("retry")
-
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Fprintf(os.Stderr, "error occurred %q \n", r)
-			cl.Usage()
-			os.Exit(1)
-		}
-	}()
-
-	cl.Usage = usage
-	for name, cfg := range compliance {
-		switch cursor := cfg.cursor.(type) {
-		case *string:
-			cl.StringVar(cursor, name, "", cfg.usage)
-		case *bool:
-			cl.BoolVar(cursor, name, false, cfg.usage)
-		}
-
-	}
-	cl.StringVar(&Timeout, "timeout", Timeout, "value which supported by time.ParseDuration")
-	cl.Parse(os.Args[1:])
-
-	timeout, err := time.ParseDuration(Timeout)
-	if err != nil {
-		panic(err)
-	}
-
-	strategies, err := handle(cl.Flags())
-	if err != nil {
-		panic(err)
-	}
-
-	args := cl.Args()
-	if len(args) == 0 {
-		panic("please provide a command to retry")
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), timeout)
-
-	return ctx, cl.Args(), strategies
 }
