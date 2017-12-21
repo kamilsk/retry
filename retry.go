f84d3b682ccef99720d6adab2ae9e5bf034dfe0c
Kamil Samigullin <kamil@samigullin.info>
2017-12-21 14:53:28 +0300

    fix #107: add tweet button and update tracking image

--- retry.go
+++ retry.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
-// Package retry provides functional mechanism based on context
+// Package retry provides functional mechanism based on channels
 // to perform actions repetitively until successful.
 //
 // This package is an extended version of https://godoc.org/github.com/Rican7/retry.
