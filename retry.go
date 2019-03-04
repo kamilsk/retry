ed78796e58bb858b29803542e0fba1c2ad3263cb
Kamil Samigullin <kamil@samigullin.info>
2019-03-04 10:02:31 +0300

    define future Interface and some aliases

--- retry.go
+++ retry.go
@@ -49,24 +49,6 @@ func TryContext(
 	return retry(ctx, currying(ctx, action), strategies...)
 }
 
-// A Breaker carries a cancellation signal to break an action execution.
-//
-// It is a subset of context.Context and github.com/kamilsk/breaker.Breaker.
-type Breaker interface {
-	// Done returns a channel that's closed when a cancellation signal occurred.
-	Done() <-chan struct{}
-}
-
-// A BreakCloser carries a cancellation signal to break an action execution
-// and can release resources associated with it.
-//
-// It is a subset of github.com/kamilsk/breaker.Breaker.
-type BreakCloser interface {
-	Breaker
-	// Close closes the Done channel and releases resources associated with it.
-	Close()
-}
-
 // IsInterrupted checks that the error is related to the Breaker interruption
 // on Retry call.
 func IsInterrupted(err error) bool {
